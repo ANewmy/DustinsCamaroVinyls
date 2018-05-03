70dc6c5da790b85b8e66003ff478babae84acb4a
Andrew Newmark <andrew@phosphorus.io>
2018-05-03 12:52:41 -0400

    Updating files

--- README.md
+++
@@ -1,1 +0,0 @@
-# DustinsCamaroVinyls
--- static/angJS/store.js
+++ static/angJS/store.js
@@ -5,40 +5,26 @@ app.controller('menuCtrl', function($scope,$http){
 	$http({
 	method: 'GET',
 	url: '/store',
-
 	//menu is to get the data from DB collection menu
 
 	}).then(function successCallback(response) {
 	$scope.menu=response.data
-	$scope.msg = "Store"
 
 	}, function errorCallback(response) {
 	$scope.menu= []
 	});
 
+	$scope.msg = "Store"
+
 
 //whenever the web page is loaded, it will pull the data from menu in DB server
 
 //second: update menu
-$scope.updateStore=function(item){
+$scope.updateMenu=function(item){
 		$http({
 	method: 'POST',
 	url: '/updateStore',
 	data: item
-
-	}).then(function successCallback(response) {
-	$scope.msg="Updated!"
-
-	}, function errorCallback(response) {
-	$scope.msg="Server problem, try again later"
-	});
-}
-
-$scope.addItem=function(){
-		$http({
-	method: 'POST',
-	url: '/addItem',
-
 
 	}).then(function successCallback(response) {
 	$scope.msg="Updated!"
